2528535e98e18786f05e8a40725e2191496cf87e
MAJDER0 <tomasz.majder89@gmail.com>
2023-11-26 14:33:46 +0100

    Hello World

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Hello World', style: TextStyle(fontSize: 20)),
+      ),
+    ),
+  ));
+}
