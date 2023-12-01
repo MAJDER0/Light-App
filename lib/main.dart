a714ff2cec2244373a6124d1449a987e6715bda7
MAJDER0 <tomasz.majder89@gmail.com>
2023-12-01 22:39:44 +0100

    preliminary layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,80 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text(''),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text('Hello World', style: TextStyle(fontSize: 20)),
-      ),
-    ),
+    home: BluetoothControlScreen(),
   ));
+}
+
+class BluetoothControlScreen extends StatefulWidget {
+  @override
+  _BluetoothControlScreenState createState() => _BluetoothControlScreenState();
+}
+
+class _BluetoothControlScreenState extends State<BluetoothControlScreen> {
+  bool isConnected = false;
+  bool isLightOn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Light App', // Zmieniono tytuł na "Light App"
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Color(0xFF161616),
+      ),
+      backgroundColor: Color(0xFF161616),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                // Handle Bluetooth connection logic here
+                setState(() {
+                  isConnected = !isConnected;
+                });
+              },
+              child: Text('CONNECT TO BLUETOOTH'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              isConnected ? 'CONNECTED' : '',
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'TURN ',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                Switch(
+                  value: isLightOn,
+                  onChanged: (value) {
+                    // Handle light switch logic here
+                    setState(() {
+                      isLightOn = value;
+                    });
+                  },
+                ),
+                Text(
+                  isLightOn ? 'ON' : 'OFF',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              'THE LIGHT IS CURRENTLY ${isLightOn ? 'ON' : 'OFF'}',
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
